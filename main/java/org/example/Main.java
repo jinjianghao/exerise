e6d6367be2028a3a499c16ff16613d578e542fe6
jinjianghao <1143412600@qq.com>
2023-12-04 14:46:15 +0800

    10

--- main/java/org/example/Main.java
+++ main/java/org/example/Main.java
@@ -1,7 +1,37 @@
-package org.example;
+package main.java.org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!0.0");
+        List list = new ArrayList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+//        for (int i = 0; i < list.size(); i++) {
+//            System.out.println(list.get(i));
+//        }
+        for(Object o : list){
+            System.out.println(o);
+        }
+//        System.out.println(list.toString());
+//        System.out.println(list.toString());
+//        System.out.println(list.toString());
+//        System.out.println(list.toString());
+//
+//
+//        List<String> list1 = new ArrayList<String>();
+//        list1.add("a");
+//        list1.add("b");
+//        list1.add("c");
+//        System.out.println(Arrays.toString(list1.toArray()));
+
     }
 }
